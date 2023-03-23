15d97dab86f4d45ad502d4c37017e6c2fd68a71c
ckp-0217 <ckp0217@foxmail.com>
2023-03-23 15:06:43 +0800

    补充Permit测试

--- w3-1/test/ERC20-test.js
+++ w3-1/test/ERC20-test.js
@@ -31,10 +31,11 @@ describe("ERC20 test", function () {
 
     });
     it("授权并且存入1000", async () => {
-        await myErc20.approve(vault.address, hre.ethers.utils.parseEther("1000"));
+        await expect(myErc20.approve(vault.address, hre.ethers.utils.parseEther("1000"))).to.emit(myErc20, "Approval").withArgs(owner.address, vault.address, hre.ethers.utils.parseEther("1000"));
         await vault.deposite(myErc20.address, hre.ethers.utils.parseEther("1000"));
         expect(Number(hre.ethers.utils.formatEther(await myErc20.balanceOf(owner.address)))).to.be.equal(99000);
         expect(Number(hre.ethers.utils.formatEther(await myErc20.balanceOf(vault.address)))).to.be.equal(1000);
+
 
 
     });
@@ -44,6 +45,46 @@ describe("ERC20 test", function () {
         expect(Number(hre.ethers.utils.formatEther(await myErc20.balanceOf(vault.address)))).to.be.equal(100);
     });
     it("使用permit", async () => {
-           });
+        //准备参数
+        const nonce = await myErc20.nonces(owner.address);
+        const chainId = await owner.getChainId();
+        const deadline = Math.floor(Date.now() / 1000) + 60 * 60; // 1 hour from now
+
+        const domain = {
+            name: await myErc20.name(),
+            version: "1",
+            chainId: chainId,
+            verifyingContract: myErc20.address,
+        };
+        
+        const types = {
+            Permit: [
+                { name: "owner", type: "address" },
+                { name: "spender", type: "address" },
+                { name: "value", type: "uint256" },
+                { name: "nonce", type: "uint256" },
+                { name: "deadline", type: "uint256" },
+            ],
+        };
+        //生成签名
+        const message = {
+            owner: owner.address,
+            spender: vault.address,
+            value: hre.ethers.utils.parseEther("10000"),
+            nonce: nonce,
+            deadline: deadline,
+        };
+
+        const signature = await owner._signTypedData(domain, types, message);
+        const { v, r, s } = ethers.utils.splitSignature(signature);
+
+        await myErc20.permit(owner.address, vault.address, hre.ethers.utils.parseEther("10000"), deadline, v, r, s);
+
+        expect(await myErc20.allowance(owner.address, vault.address)).to.equal(hre.ethers.utils.parseEther("10000"));
+        await vault.deposite(myErc20.address, hre.ethers.utils.parseEther("10000"));
+        expect(Number(hre.ethers.utils.formatEther(await myErc20.balanceOf(owner.address)))).to.be.equal(89900);
+
+    });
+
 });
 
